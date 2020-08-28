cfb45e259f41729fd29791ce0e37855bd43189b6
Johann Woelper <woelper@gmail.com>
2020-08-28 22:51:36 +0200

    New tests: executable and real world test

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,12 @@
-use std::fs::copy;
-use std::path::{Path, PathBuf};
-use walkdir::WalkDir;
 use log::*;
-use std::time::SystemTime;
+use std::fs::copy;
 use std::io::{Error, ErrorKind};
+use std::path::{Path, PathBuf};
+use std::time::SystemTime;
+use walkdir::WalkDir;
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-    use std::fs::create_dir_all;
-
-    #[test]
-    fn copy() {
-        let sourcepath = "source/level1/level2/level3";
-        create_dir_all(sourcepath).unwrap();
-        std::fs::write("source/test", vec![]).unwrap();
-        std::fs::write("source/level1/other_file", vec![]).unwrap();
-
-        DirCopy::new(
-            &Path::new("source").canonicalize().unwrap(),
-            &Path::new("dest"),
-        )
-        .overwrite(true)
-        .overwrite_if_newer(true)
-        .build()
-        .unwrap();
-
-        // clean up
-        std::fs::remove_dir_all("source").unwrap();
-        std::fs::remove_dir_all("dest").unwrap();
-    }
-}
+mod tests;
 
 #[derive(Debug, Clone)]
 /// Recursively copy a directory from a to b.
@@ -43,29 +19,27 @@ pub struct DirCopy {
     overwrite_if_newer: bool,
     overwrite_if_size_differs: bool,
     exclude_filters: Vec<String>,
-    include_filters: Vec<String>
+    include_filters: Vec<String>,
 }
 
 /// Determine if the modification date of file_a is newer than that of file_b
 fn is_file_newer(file_a: &Path, file_b: &Path) -> bool {
     match (file_a.metadata(), file_b.metadata()) {
         (Ok(meta_a), Ok(meta_b)) => {
-            meta_a.modified().unwrap_or(SystemTime::now()) > meta_b.modified().unwrap_or(SystemTime::UNIX_EPOCH) 
-        },
-        _ => false
+            meta_a.modified().unwrap_or_else(|_| SystemTime::now())
+                > meta_b.modified().unwrap_or(SystemTime::UNIX_EPOCH)
+        }
+        _ => false,
     }
 }
 
-/// Determine if file_a and file_b's size differs. 
+/// Determine if file_a and file_b's size differs.
 fn is_filesize_different(file_a: &Path, file_b: &Path) -> bool {
     match (file_a.metadata(), file_b.metadata()) {
-        (Ok(meta_a), Ok(meta_b)) => {
-            meta_a.len() != meta_b.len() 
-        },
-        _ => false
+        (Ok(meta_a), Ok(meta_b)) => meta_a.len() != meta_b.len(),
+        _ => false,
     }
 }
-
 
 impl DirCopy {
     pub fn new(source: &Path, destination: &Path) -> DirCopy {
@@ -76,7 +50,7 @@ impl DirCopy {
             overwrite_if_newer: false,
             overwrite_if_size_differs: false,
             exclude_filters: vec![],
-            include_filters: vec![]
+            include_filters: vec![],
         }
     }
 
@@ -84,7 +58,7 @@ impl DirCopy {
     pub fn overwrite(self, overwrite: bool) -> DirCopy {
         DirCopy {
             overwrite_all: overwrite,
-            ..self.clone()
+            ..self
         }
     }
 
@@ -92,15 +66,15 @@ impl DirCopy {
     pub fn overwrite_if_newer(self, overwrite_only_newer: bool) -> DirCopy {
         DirCopy {
             overwrite_if_newer: overwrite_only_newer,
-            ..self.clone()
+            ..self
         }
     }
 
     /// Overwrite if size between source and dest differs (off by default)
     pub fn overwrite_if_size_differs(self, overwrite_if_size_differs: bool) -> DirCopy {
         DirCopy {
-            overwrite_if_size_differs: overwrite_if_size_differs,
-            ..self.clone()
+            overwrite_if_size_differs,
+            ..self
         }
     }
 
@@ -110,40 +84,49 @@ impl DirCopy {
         filters.push(f.to_owned());
         DirCopy {
             exclude_filters: filters,
-            ..self.clone()
+            ..self
         }
     }
-    
+
     /// Only copy files that contain this string.
     pub fn with_include_filter(self, f: &str) -> DirCopy {
         let mut filters = self.exclude_filters.clone();
         filters.push(f.to_owned());
         DirCopy {
             include_filters: filters,
-            ..self.clone()
+            ..self
         }
     }
 
     /// Execute the copy operation
     pub fn build(&self) -> Result<(), std::io::Error> {
-        
         if !self.destination.is_dir() {
             debug!("MKDIR {:?}", &self.destination);
             std::fs::create_dir_all(&self.destination)?;
         }
         let abs_source = self.source.canonicalize()?;
         let abs_dest = self.destination.canonicalize()?;
-        debug!("Building copy operation: SRC {} DST {}", abs_source.display(), abs_dest.display());
+        debug!(
+            "Building copy operation: SRC {} DST {}",
+            abs_source.display(),
+            abs_dest.display()
+        );
 
         for entry in WalkDir::new(&abs_source).into_iter().filter_map(|e| e.ok()) {
-            let rel_dest = entry.path().strip_prefix(&abs_source).map_err(|e | Error::new(ErrorKind::Other, format!("Could not strip prefix: {:?}",e)) )?;
+            let rel_dest = entry.path().strip_prefix(&abs_source).map_err(|e| {
+                Error::new(ErrorKind::Other, format!("Could not strip prefix: {:?}", e))
+            })?;
             let dest_entry = abs_dest.join(rel_dest);
 
-            if entry.path().is_file() { // the source exists
+            if entry.path().is_file() {
+                // the source exists
 
-                
                 // Early out if target is present and overwrite is off
-                if !self.overwrite_all && dest_entry.is_file() && !self.overwrite_if_newer && !self.overwrite_if_size_differs {
+                if !self.overwrite_all
+                    && dest_entry.is_file()
+                    && !self.overwrite_if_newer
+                    && !self.overwrite_if_size_differs
+                {
                     continue;
                 }
 
@@ -152,24 +135,32 @@ impl DirCopy {
                         continue;
                     }
                 }
-                
+
                 for f in &self.include_filters {
                     if !entry.path().to_string_lossy().contains(f) {
                         continue;
-                     }
+                    }
                 }
 
                 // File is not present: copy it
                 if !dest_entry.is_file() {
-                    debug!("Dest not present: CP {} DST {}", entry.path().display(), dest_entry.display());
+                    debug!(
+                        "Dest not present: CP {} DST {}",
+                        entry.path().display(),
+                        dest_entry.display()
+                    );
                     copy(entry.path(), dest_entry)?;
                     continue;
                 }
 
                 // File newer?
-                if self.overwrite_if_newer { 
+                if self.overwrite_if_newer {
                     if is_file_newer(entry.path(), &dest_entry) {
-                        debug!("Source newer: CP {} DST {}", entry.path().display(), dest_entry.display());
+                        debug!(
+                            "Source newer: CP {} DST {}",
+                            entry.path().display(),
+                            dest_entry.display()
+                        );
                         copy(entry.path(), &dest_entry)?;
                     }
                     continue;
@@ -178,18 +169,19 @@ impl DirCopy {
                 // Different size?
                 if self.overwrite_if_size_differs {
                     if is_filesize_different(entry.path(), &dest_entry) {
-                        debug!("Source differs: CP {} DST {}", entry.path().display(), dest_entry.display());
+                        debug!(
+                            "Source differs: CP {} DST {}",
+                            entry.path().display(),
+                            dest_entry.display()
+                        );
                         copy(entry.path(), &dest_entry)?;
                     }
                     continue;
                 }
-                     
-                
+
                 // The regular copy operation
                 debug!("CP {} DST {}", entry.path().display(), dest_entry.display());
                 copy(entry.path(), dest_entry)?;
-            
-            
             } else if entry.path().is_dir() && !dest_entry.is_dir() {
                 debug!("MKDIR {}", entry.path().display());
                 std::fs::create_dir_all(dest_entry)?;
---
+++ src/tests.rs
@@ -0,0 +1,79 @@
+use super::*;
+use std::fs::create_dir_all;
+use std::fs::File;
+#[cfg(unix)]
+use std::os::unix::fs::PermissionsExt;
+
+#[test]
+fn copy() {
+    create_dir_all("source/level1/level2/level3").unwrap();
+    File::create("source/test").unwrap();
+    File::create("source/level1/other_file").unwrap();
+
+    #[cfg(unix)]
+    {
+        File::create("source/exec_file").unwrap();
+        std::fs::set_permissions("source/exec_file", std::fs::Permissions::from_mode(0o755))
+            .unwrap();
+    }
+
+    DirCopy::new(
+        &Path::new("source").canonicalize().unwrap(),
+        &Path::new("dest"),
+    )
+    .overwrite(true)
+    .overwrite_if_newer(true)
+    .build()
+    .unwrap();
+
+    #[cfg(unix)]
+    {
+        let f = File::open("dest/exec_file").unwrap();
+        let metadata = f.metadata().unwrap();
+        let permissions = metadata.permissions();
+        println!("permissions: {:o}", permissions.mode());
+        assert_eq!(permissions.mode(), 33261);
+    }
+
+    // clean up
+    std::fs::remove_dir_all("source").unwrap();
+    std::fs::remove_dir_all("dest").unwrap();
+}
+
+#[test]
+fn kernel() {
+    let url = "https://github.com/rust-lang/cargo/archive/master.zip";
+    let sample_dir = "sample";
+    let output_dir = format!("{}_output", sample_dir);
+    let archive = format!("{}.zip", sample_dir);
+
+    let mut resp = reqwest::blocking::get(url).unwrap();
+    let mut out = File::create(&archive).expect("failed to create file");
+    std::io::copy(&mut resp, &mut out).expect("failed to copy content");
+
+    let reader = std::fs::File::open(&archive).unwrap();
+
+    unzip::Unzipper::new(reader, sample_dir).unzip().unwrap();
+    let num_input_files = WalkDir::new(&sample_dir)
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .count();
+
+    DirCopy::new(
+        &Path::new(sample_dir).canonicalize().unwrap(),
+        &Path::new(&output_dir),
+    )
+    .build()
+    .unwrap();
+
+    let num_output_files = WalkDir::new(&output_dir)
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .count();
+
+    assert_eq!(num_output_files, num_input_files);
+
+    std::fs::remove_dir_all(sample_dir).unwrap();
+    std::fs::remove_dir_all(output_dir).unwrap();
+    std::fs::remove_file(archive).unwrap();
+}
