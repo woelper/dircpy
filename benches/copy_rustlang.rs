bb3969ba85da2b547a98d061207c7c4f1fca5450
Johann Woelper <woelper@gmail.com>
2020-09-02 22:46:19 +0200

    better tests

--- benches/copy_rustlang.rs
+++ benches/copy_rustlang.rs
@@ -5,144 +5,110 @@ use std::fs::File;
 use std::path::Path;
 use dircpy::CopyBuilder;
 
-fn bench_rustlang_copy() {
+
+
+
+
+fn download_and_unpack(url: &str, name: &str) {
+    // let url = "";
+    let sample_dir = name;
+    let archive = format!("{}.zip", sample_dir);
+
+    if !std::path::Path::new(&archive).is_file() {
+        info!("Downloading {:?}", url);
+    
+        let mut resp = reqwest::blocking::get(url).unwrap();
+        let mut out = File::create(&archive).expect("failed to create file");
+        std::io::copy(&mut resp, &mut out).expect("failed to copy content");
+        
+    }
+
+    info!("Unzipping...");
+    let reader = std::fs::File::open(&archive).unwrap();
+    unzip::Unzipper::new(reader, sample_dir).unzip().unwrap();
+    info!("Done. Ready.");
+
+}
+
+
+fn test_cp(c: &mut Criterion) {
     std::env::set_var("RUST_LOG", "INFO");
-
     let _ = env_logger::builder()
-        // .is_test(true)
         .try_init();
+    // One-time setup code goes here
+    let source = "rustlang";
+    let dest = "test";
+    download_and_unpack("https://github.com/rust-lang/rust/archive/master.zip", source);
+    c.bench_function("cp -r", |b| {
+        // Per-sample (note that a sample can be many iterations) setup goes here
+        b.iter(|| {
+            // Measured code goes here
+            std::process::Command::new("cp").arg("-r").arg(source).arg(dest).output();
 
-    let url = "https://github.com/rust-lang/rust/archive/master.zip";
-    let sample_dir = "rustlang";
-    //let output_dir = format!("{}_output", sample_dir);
-    let archive = format!("{}.zip", sample_dir);
-
-    info!("Downloading {:?}", url);
-
-    let mut resp = reqwest::blocking::get(url).unwrap();
-    let mut out = File::create(&archive).expect("failed to create file");
-    std::io::copy(&mut resp, &mut out).expect("failed to copy content");
-    info!("Unzipping...");
-
-    let reader = std::fs::File::open(&archive).unwrap();
-    unzip::Unzipper::new(reader, sample_dir).unzip().unwrap();
-    
-    let iterations = 6;
-    let mut complete_duration = std::time::Duration::from_nanos(0);
-    for i in 0..iterations {
-        info!("Iteration {}/{}", i, iterations);
-        let start = std::time::Instant::now();
-        
-        CopyBuilder::new(
-            &Path::new(sample_dir).canonicalize().unwrap(),
-            &format!("output_{}", i),
-        )
-        .run()
-        .unwrap();
-    
-        info!("Elapsed time: {:?}", start.elapsed());
-        complete_duration += start.elapsed();
-    }
-
-    info!("Avg time: {:?}", complete_duration/iterations);
-    
-    info!("Cleanup");
-    std::fs::remove_dir_all(sample_dir).unwrap();
-    std::fs::remove_file(archive).unwrap();
-    for i in 0..iterations {
-        std::fs::remove_dir_all(&format!("output_{}", i)).unwrap();
-    }
-    info!("Done");
-}
-
-
-fn bench_copy(src: &str, dest: &str) {
-
-        CopyBuilder::new(
-            &src,
-            &dest,
-        )
-        .run()
-        .unwrap();
-    
-    }
-    
-
-
-
-
-
-fn fibonacci(n: u64) -> u64 {
-    match n {
-        0 => 1,
-        1 => 1,
-        n => fibonacci(n-1) + fibonacci(n-2),
-    }
-}
-
-fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("fib 20", |b| b.iter(|| fibonacci(black_box(20))));
-}
-
-fn cargo_benchmark(c: &mut Criterion) {
-    
-    let url = "https://github.com/rust-lang/cargo/archive/master.zip";
-
-    // let url = "https://github.com/rust-lang/rust/archive/master.zip";
-    let sample_dir = "rustlang";
-    //let output_dir = format!("{}_output", sample_dir);
-    let archive = format!("{}.zip", sample_dir);
-
-    info!("Downloading {:?}", url);
-
-    let mut resp = reqwest::blocking::get(url).unwrap();
-    let mut out = File::create(&archive).expect("failed to create file");
-    std::io::copy(&mut resp, &mut out).expect("failed to copy content");
-    info!("Unzipping...");
-
-    let reader = std::fs::File::open(&archive).unwrap();
-    unzip::Unzipper::new(reader, sample_dir).unzip().unwrap();
-    let input = 5u64;
-    //c.bench_with_input(BenchmarkId::new("function_name", input), &input |b,i| b.iter(|| bench_copy("rustlang", &format!("output_{}",i))));
-
-    // c.bench_with_input(
-    //     BenchmarkId::new("copy rustlang", input), &input,
-    //     |b, i| b.iter(|| {
-    //         // Code to benchmark using input `i` goes here
-    //         bench_copy(sample_dir, &format!("output_{}",i))
-
-    //     }),
-    // );
-
-    let mut group = c.benchmark_group("from_elem");
-    for size in 0..input {
-        
-        group.bench_with_input(BenchmarkId::from_parameter(size), &size, |b, &size| {
-            b.iter(|| {
-                bench_copy(sample_dir, &format!("output_{}", size))
-
-            });
         });
-    }
+    });
 
-    info!("Cleanup");
-    std::fs::remove_dir_all(sample_dir).unwrap();
-    std::fs::remove_file(archive).unwrap();
-    for i in 0..=input {
-        std::fs::remove_dir_all(&format!("output_{}", i));
-    }
-    info!("Done");
+    std::fs::remove_dir_all(source).unwrap();
+    std::fs::remove_dir_all(dest).unwrap();
 }
 
-fn rustlang_benchmark(c: &mut Criterion) {
-    
-   bench_rustlang_copy();
+
+fn test_cpy(c: &mut Criterion) {
+    std::env::set_var("RUST_LOG", "INFO");
+    let _ = env_logger::builder()
+        .try_init();
+    // One-time setup code goes here
+    let source = "rustlang";
+    let dest = "test";
+    download_and_unpack("https://github.com/rust-lang/rust/archive/master.zip", source);
+    c.bench_function("cpy", |b| {
+        // Per-sample (note that a sample can be many iterations) setup goes here
+        b.iter(|| {
+            // Measured code goes here
+            CopyBuilder::new(
+                &source,
+                &dest,
+            )
+            .overwrite(true)
+            .run()
+            .unwrap();
+        });
+    });
+
+    std::fs::remove_dir_all(source).unwrap();
+    std::fs::remove_dir_all(dest).unwrap();
+}
+
+
+fn test_lms(c: &mut Criterion) {
+    std::env::set_var("RUST_LOG", "INFO");
+    let _ = env_logger::builder()
+        .try_init();
+    // One-time setup code goes here
+    let source = "rustlang";
+    let dest = "test";
+    download_and_unpack("https://github.com/rust-lang/rust/archive/master.zip", source);
+    c.bench_function("lms", |b| {
+        // Per-sample (note that a sample can be many iterations) setup goes here
+        b.iter(|| {
+            // Measured code goes here
+            std::process::Command::new("lms").arg("cp").arg(source).arg(dest).output();
+
+        });
+    });
+
+    std::fs::remove_dir_all(source).unwrap();
+    std::fs::remove_dir_all(dest).unwrap();
 }
 
 criterion_group!{
     name = benches;
     // This can be any expression that returns a `Criterion` object.
-    config = Criterion::default().sample_size(10);
-    targets = rustlang_benchmark
+    config = Criterion::default()
+    .sample_size(10)
+    .warm_up_time(std::time::Duration::from_secs(3))
+    .measurement_time(std::time::Duration::from_secs(30))
+    ;
+    targets = test_cpy, test_cp, test_lms
 }
 criterion_main!(benches);
